64b84da686328dc29e6107f0c56d587dac3d8193
Michael Arvelo <michaeldevelopscode@gmail.com>
2020-11-13 16:46:17 -0500

    added timestamps for timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
 
 
 // Add current day to header
-$("#currentDay").text(moment().format('dddd, MMMM Do'));
+var getDate = moment().format('dddd, MMMM Do');
+$("#currentDay").text(getDate);
 
+// add timeblocks for work hours(8 hours: 9-5pm)
+for (let i = 0; i <= 8; i++) {
+  // make timeBlock
+  var timeBlock = $('<div>')
+    .addClass('row')
+    .attr('data-hour',i);
+
+  // make div for hour <p>
+  var hourWrapper = $('<div>')
+    .addClass('row')
+    .addClass('col-2')
+    .addClass('justify-content-end')
+    .addClass('hour');
+
+  // make hour element <p>
+  var hourEl = $('<p>')
+    .addClass('align-self-center');
+
+  // add 9 to show correct hour
+  var correctHour = 9 + i;
+
+  // format to 12hr
+  var showHour = moment(correctHour, 'hA').format('hA');
+
+  // add showHour to element text
+  hourEl.text(showHour);
+
+  // add hour element to hourWrapper
+  hourWrapper.append(hourEl);
+
+  // add to timeblock
+  timeBlock.append(hourWrapper);
+
+  // append to timeblock container on the page
+  $(".container").append(timeBlock);  
+};
