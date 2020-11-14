ac30208f43c154f86c44bb5c923cbef3b40ec860
Michael Arvelo <michaeldevelopscode@gmail.com>
2020-11-13 19:04:44 -0500

    added planner input/textarea and created seperate timeBlock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,21 @@
 var getDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(getDate);
 
-// Make timestamps function
-var createTimeStamps = function(i) {
+var createTimeBlock = function(i) {
     // make timeBlock
     var timeBlock = $('<div>')
         .addClass('row')
         .addClass('time-block')
+        .attr('id', 'block-'+i)
         .attr('data-hour',i);
+
+    // append to timeblock container on the page
+    $(".container").append(timeBlock);  
+}; 
+
+// Make timestamps function
+var createTimeStamps = function(i) {
+
     // make div for hour <p>
     var hourWrapper = $('<div>')
         .addClass('row')
@@ -35,17 +43,54 @@ var createTimeStamps = function(i) {
     hourWrapper.append(hourEl);
 
     // add to timeblock
-    timeBlock.append(hourWrapper);
-
-    // append to timeblock container on the page
-    $(".container").append(timeBlock);  
+    $('#block-'+i).attr('data-hour', i).append(hourWrapper);
 };
 
+// Make planner input function
+var createPlannerInputs = function(i) {
+    // add 9 to show correct hour from timestamps again
+    var timeStamp = i + 9;
+    // make planner input and add attributes
+    var plannerInput = $('<textarea>')
+        .addClass()
+        .attr('id','input-'+i)
+        .attr('type','text');
+    
+    // grab time in hours (24hr format)
+    var currentHour = moment().format('H');
+    // find hours diff by subtraction
+    var timeDiff = timeStamp - currentHour;
+    console.log(timeDiff);
 
+    // add class depending on time (color code)
+    if (timeDiff < 0) { // past time class
+        plannerInput.addClass('past'); 
+    } else if(timeDiff === 0) { // present time class
+        plannerInput.addClass('present'); 
+    } else if(timeDiff > 0) { // future time class
+        plannerInput.addClass('future');
+    }
+
+    // make new col for row
+    var plannerInputWrapper = $('<div>')
+        .addClass('col-8')
+        .addClass('description');
+    
+    // add input element to div Wrapper
+    plannerInputWrapper.append(plannerInput);
+
+    // add to timeblock
+    $('#block-'+i).attr('data-hour', i).append(plannerInputWrapper);
+
+};
 
 // add timeblocks for work hours(8 hours: 9-5pm)
 for (let i = 0; i <= 8; i++) {
-    // Call time stamp function
+    // call time block function
+    createTimeBlock(i);
+    // call time stamp function
     createTimeStamps(i);
+    // call function to create the planner input
+    createPlannerInputs(i);
     
 };
