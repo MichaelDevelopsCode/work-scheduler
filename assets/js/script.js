a968828707ff7bac76ff61b75dd8c4edd03dcf24
Michael Arvelo <michaeldevelopscode@gmail.com>
2020-11-14 13:13:27 -0500

    added save and load local storage on save btn click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
-
-
 // Add current day to header
 var getDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(getDate);
+
+var loadSchedule = function(i) {
+    var schedule = localStorage.getItem(i);
+
+    // if nothing in localStorage return
+    if (schedule === null) {
+        return false;
+    } else {
+        return schedule;
+    }
+}
+
 
 var createTimeBlock = function(i) {
     // make timeBlock
@@ -48,11 +58,12 @@ var createTimeStamps = function(i) {
 
 // Make planner input function
 var createPlannerInputs = function(i) {
-    // add 9 to show correct hour from timestamps again
+    // add 9 to show correct hour from timestamps
     var timeStamp = i + 9;
     // make planner input and add attributes
     var plannerInput = $('<textarea>')
-        .addClass()
+        .addClass('col-8')
+        .addClass('description')
         .attr('id','input-'+i)
         .attr('type','text');
     
@@ -60,7 +71,6 @@ var createPlannerInputs = function(i) {
     var currentHour = moment().format('H');
     // find hours diff by subtraction
     var timeDiff = timeStamp - currentHour;
-    console.log(timeDiff);
 
     // add class depending on time (color code)
     if (timeDiff < 0) { // past time class
@@ -71,26 +81,70 @@ var createPlannerInputs = function(i) {
         plannerInput.addClass('future');
     }
 
-    // make new col for row
-    var plannerInputWrapper = $('<div>')
-        .addClass('col-8')
-        .addClass('description');
-    
-    // add input element to div Wrapper
-    plannerInputWrapper.append(plannerInput);
+    // add text from local storage if avail
+    var addSavedEvent = loadSchedule(i);
+
+    if(addSavedEvent) {
+        plannerInput.val(addSavedEvent);
+    } else {
+        plannerInput.val("");
+    }
 
     // add to timeblock
-    $('#block-'+i).attr('data-hour', i).append(plannerInputWrapper);
-
+    $('#block-'+i).attr('data-hour', i).append(plannerInput);
 };
+
+var createSaveBtn = function(i) {
+    // make save button
+    var saveBtn = $('<div>')
+        .addClass('saveBtn')
+        .addClass('col-2')
+        .addClass('row')
+        .addClass('justify-content-center')
+        .addClass('align-items-center')
+        .attr('id','btn-'+i);
+    
+    // add icons
+    var saveBtnIcon = $('<i>')
+        .addClass('fas fa-save')
+        .addClass('btnClick');
+
+    // add icons to btn
+    saveBtn.append(saveBtnIcon);
+
+    // add btn to timeblock
+    $('#block-'+i).attr('data-hour', i).append(saveBtn);
+}
 
 // add timeblocks for work hours(8 hours: 9-5pm)
 for (let i = 0; i <= 8; i++) {
-    // call time block function
+    // call function to create time block
     createTimeBlock(i);
-    // call time stamp function
+    // call function to create time stamp
     createTimeStamps(i);
     // call function to create the planner input
     createPlannerInputs(i);
-    
+    // call function to create save buttons/icons
+    createSaveBtn(i);
 };
+
+
+var saveSchedule = function(textId, text) {
+    // add new object with task id and value of the text
+    localStorage.setItem(textId, text);
+};
+
+$('.btnClick').on('click', function() {
+    // get text id (same as btn clicked)
+    var textId = $(this)
+         .closest('.saveBtn')
+         .attr('id')
+         .replace('btn-','');
+
+    // grab text
+    var text = $('#input-'+textId)
+        .val();
+
+    saveSchedule(textId, text);
+    
+});
